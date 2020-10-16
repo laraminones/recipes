eeffbb23740a0d67595a9a866998571f99f2c494
LoisSotoLopez <loissotolopez@gmail.com>
2020-10-16 11:44:06 +0200

    Test basic spider crawling

--- recipes/recipes/spiders/rec_spider.py
+++ recipes/recipes/spiders/rec_spider.py
@@ -5,15 +5,15 @@ class QuotesSpider(scrapy.Spider):
 
 	def start_requests(self):
 		urls = [
-			'https://es.wikipedia.org/wiki/Wikipedia:Portada'
+			'https://www.recipetineats.com/recipes/?fwp_paged=1'
 		]
 
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
 
 	def parse(self, response):
-		page = response.url.split("/")[-2]
+		page = response.url.split("=")[-1]
 		filename = f'recipes-{page}.html'
 		with open(filename, 'wb') as f:
-			f.write(response.body)
-		self.flog(f'saved file {filename}')
+			for link in response.css('entry-title-link'):
+				f.write('link :' + link.attrib['href'].get())
