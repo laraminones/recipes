eef4ba0fe476879ca9a8e831f7c2d99b1e33cf2c
Lara Miñones <lara.minones@udc.es>
2020-10-16 13:33:00 +0200

    Add recipe relevant information crawling

--- recipes/recipes/spiders/rec_spider.py
+++ recipes/recipes/spiders/rec_spider.py
@@ -13,8 +13,24 @@ class QuotesSpider(scrapy.Spider):
 
 	def parse(self, response):
 		page = response.url.split("=")[-1]
-		filename = f'recipes-{page}.html'
-		with open(filename, 'wb') as f:
-			for link in [link.attrib['href'] in response.css('.entry-title-link').getall():
-				self.log("--------------------------")
-				self.log(link)
+		for link in [link.attrib['href'] for link in response.css('.entry-title-link')]:
+			meta = {
+				'recipe_url': link
+			}
+			yield scrapy.Request(url=link, callback=self.parse_recipe, meta=meta)
+
+	def parse_recipe(self, response):
+		rec_title = response.css('.wprm-recipe-name::text')[0]
+		rec_prep_time = response.css('.wprm-recipe-prep_time::text')[0]
+		rec_cook_time = response.css('.wprm-recipe-cook_time::text')[0]
+		#rec_servings = response.css('.wprm-recipe-servings::text')[0]
+		rec_ingredients = response.css('.wprm-recipe-ingredient-name::text').getall()
+		#rec_ingredients_amount = response.css('.wprm-recipe-ingredient-amount::text').getall()
+		rec_instructions = response.css('.wprm-recipe-instruction-text > span::text').getall()
+
+		self.log(rec_title)
+		self.log(rec_prep_time)
+		self.log(rec_cook_time)
+		self.log(rec_ingredients)
+		self.log(rec_instructions)
+
