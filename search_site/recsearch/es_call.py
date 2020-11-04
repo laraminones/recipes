1433314a44d9702eab0f59c7ab1f11e28778a021
Lara Miñones <lara.minones@udc.es>
2020-11-04 12:43:53 +0100

    Adding jsonlines

--- search_site/recsearch/es_call.py
+++ search_site/recsearch/es_call.py
@@ -4,9 +4,11 @@ from elasticsearch_dsl import Search, Q
 def recsearch(rec_prep_time=""):
     client = Elasticsearch()
     #q = Q("bool", should=Q("match", rec_prep_time=rec_prep_time), minimum_should_match=1)
-    q = Q("match", rec_prep_time='20')
-    s = Search(index="recipes_index").using(client)#.query(q)
+    q = Q("match", recipes_index__rec_prep_time=rec_prep_time)
+    s = Search(index="recipes_index").using(client).query(q)
+    #s.filter('term', rec_prep_time__keyword="20")
     response = s.execute()
+    print(response)
     print('Total %s hits found.' % response.hits.total.value)
     search = get_results(response)
     return search
@@ -14,7 +16,9 @@ def recsearch(rec_prep_time=""):
 def get_results(response):
     results = []
     for hit in response:
-        results.append(hit.rec_title)
+        for i in range(len(hit.recipes_index)):
+            results.append(hit.recipes_index[i].rec_title)
+            i=i+1
     return results
 
 if __name__ == '__main__':  
