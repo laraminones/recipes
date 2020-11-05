791a351440ed6594d0ffb5049b0215b3924ae9e3
Lara Miñones <lara.minones@udc.es>
2020-11-05 10:38:46 +0100

    Add web page first approach

--- search_site/recsearch/views.py
+++ search_site/recsearch/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .es_call import recsearch
 
-def search_index(request): 
-    return HttpResponse("Awesome search coming soon!")
+def search_index(request):
+	results = []
+	rec_prep_time_term = ""
+
+	if request.GET.get('rec_prep_time'):
+		rec_prep_time_term = request.GET['rec_prep_time']
+
+	search_term = rec_prep_time_term
+	results = recsearch(rec_prep_time=rec_prep_time_term)
+	print(results)
+	context = {'results': results, 'count': len(results), 'search_term': search_term}
+
+	return render(request, 'recsearch/index.html', context)
